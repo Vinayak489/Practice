17e685627c793366e6276500979f4493716e2233
Vinayak489 <vinayaksmaf58@gmail.com>
2024-08-02 18:21:24 +0530

    v

---
+++ Fullstack/Frontend/language/typescript/Data types/own data types/own data types/Human/bike/test.ts
@@ -0,0 +1,15 @@
+class Bike{
+    handel=1
+    mirror=2
+    gears=5
+    wheel=2
+    
+    withwheel(){
+        return this.wheel
+    }
+    
+    
+    
+    }
+    let deep=new Bike()
+    console.log(deep)
